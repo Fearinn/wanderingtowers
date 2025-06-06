8be284cf978a6353caadde4e54a6ab461dcb9c1e
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2025-06-06 18:06:04 -0300

    refactor: wrap panel elements in whiteblocks

--- src/wanderingtowers.ts
+++ src/wanderingtowers.ts
@@ -164,14 +164,14 @@ class WanderingTowers extends WanderingTowersGui {
 
       playerPanelElement.insertAdjacentHTML(
         "beforeend",
-        `<div id="wtw_ravenskeepCounter-${player_id}" class="wtw_ravenskeepCounter">
+        `<div id="wtw_ravenskeepCounter-${player_id}" class="wtw_whiteblock wtw_ravenskeepCounter">
           <div id="wtw_ravenskeepCounterIcon" class="wtw_ravenskeepCounterIcon"></div>
           <div class="wtw_ravenskeepCountContainer">
             <span id="wtw_ravenskeepCount" class="wtw_ravenskeepCount">0</span>
             <span id="wtw_ravenskeepGoal" class="wtw_ravenskeepGoal"> / ${gamedatas.ravenskeepGoal}</span>
           </div>
         </div>
-        <div id="wtw_potionCargo-${player_id}" class="wtw_potionCargo"></div>`
+        <div id="wtw_potionCargo-${player_id}" class="wtw_whiteblock wtw_potionCargo"></div>`
       );
 
       counters[player_id] = {
